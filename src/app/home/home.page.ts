e3e6358aaf267c4e66bf5d8ab5c99c8aabff20e6
Elena Baldaeva <eelenaea@gmail.com>
2026-05-08 19:08:30 +0100

    Implemented Home Page UI and fixed HttpClient provider

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  trendingMovies: any[] = [];
+  studentNumber: string = 'G00472875';
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.loadTrendingMovies();
+  }
+
+  loadTrendingMovies() {
+    this.movieService.getTrendingMovies().subscribe((data) => {
+      this.trendingMovies = data.results;
+      console.log(this.trendingMovies);
+    });
+  }
 }
